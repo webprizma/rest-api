55a5925e0f43bc2c672831e5491a2430135760eb
Yuri Kulagin <kulagin1987@gmail.com>
2022-08-21 15:03:52 +0300

    refactor

---
+++ src/test/java/models/SingleUserDataModel.java
@@ -0,0 +1,8 @@
+package models;
+
+import lombok.Data;
+
+@Data
+public class SingleUserDataModel {
+    private SingleUserModel data;
+}
--- src/test/java/models/SingleUserModel.java
+++ src/test/java/models/SingleUserModel.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class SingleUserModel {
-    private String dataEmail;
+    private String email;
 }
